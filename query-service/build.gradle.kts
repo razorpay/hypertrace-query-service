095071f63c16cd41b76ed2492174081d15d1dc5d
Harnoor-se7en <100941492+Harnoor-se7en@users.noreply.github.com>
2022-04-26 20:05:51 +0530

    Update build.gradle.kts

--- query-service/build.gradle.kts
+++ query-service/build.gradle.kts
@@ -61,7 +61,7 @@ hypertraceDocker {
 }
 
 fun getCommitHash(): String {
-  return "0dcce420277ecb38af1fcebfb1da6ab2ed7ad7fe"
+  return System.getenv("GITHUB_SHA_SHORT").toString()
 }
 
 tasks.jacocoIntegrationTestReport {
