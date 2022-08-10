6f36a1c7e011ed83e72a8c2687440d1593370f84
13shivam <shivam.rai@razorpay.com>
2022-08-10 18:44:13 +0530

    upgrade depe

--- query-service-impl/build.gradle.kts
+++ query-service-impl/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.exclude
-
 plugins {
   `java-library`
   jacoco
@@ -36,6 +34,11 @@ dependencies {
   implementation("org.hypertrace.core.serviceframework:service-framework-spi:0.1.33")
   implementation("com.google.inject:guice:5.0.1")
   implementation(files("../patch/pinot-java-client-0.6.0-tls.jar"))
+  implementation ("com.fasterxml.jackson.core:jackson-databind:2.13.1")
+//  implementation("org.apache.pinot:pinot-java-client:0.6.0") {
+//    // We want to use log4j2 impl so exclude the log4j binding of slf4j
+//    exclude("org.slf4j", "slf4j-log4j12")
+//  }
   implementation("org.slf4j:slf4j-api:1.7.32")
   implementation("commons-codec:commons-codec:1.15")
   implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.33")
