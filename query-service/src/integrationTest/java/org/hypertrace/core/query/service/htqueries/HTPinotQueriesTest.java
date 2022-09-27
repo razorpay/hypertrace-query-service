7b6e6354ec7831b9a39e5006ccb0601cae4bed6a
tripti.tripathi <tripti.tripathi@razorpay.com>
2022-09-27 16:16:26 +0530

    fix

--- query-service/src/integrationTest/java/org/hypertrace/core/query/service/htqueries/HTPinotQueriesTest.java
+++ query-service/src/integrationTest/java/org/hypertrace/core/query/service/htqueries/HTPinotQueriesTest.java
@@ -252,7 +252,7 @@ public class HTPinotQueriesTest {
     // stop this service
     viewGen.stop();
 
-    return retry < 5;
+    return retry < 10;
   }
 
   private static boolean areMessagesConsumed(Map<String, Long> endOffSetMap) throws Exception {
