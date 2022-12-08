15f23212a09435476f3a22266c3ed1609fc70550
Harnoor Singh <harnoor.singh@razorpay.com>
2022-12-08 19:30:11 +0530

    feat:change start time filter

--- query-service-impl/src/main/java/org/hypertrace/core/query/service/AbstractQueryTransformation.java
+++ query-service-impl/src/main/java/org/hypertrace/core/query/service/AbstractQueryTransformation.java
@@ -113,8 +113,21 @@ public abstract class AbstractQueryTransformation implements QueryTransformation
     if (filter.equals(Filter.getDefaultInstance())) {
       return Single.just(filter);
     }
+    Expression lhsExpression = filter.getLhs();
+    if (lhsExpression.getValueCase() == Expression.ValueCase.ATTRIBUTE_EXPRESSION) {
+      String lhsAttributeId = lhsExpression.getAttributeExpression().getAttributeId();
+      if (lhsAttributeId.equals("API.startTime") || lhsAttributeId.equals("SERVICE.startTime")) {
+        lhsExpression =
+                Expression.newBuilder()
+                        .setAttributeExpression(
+                                AttributeExpression.newBuilder()
+                                        .setAttributeId(lhsAttributeId.concat("Filter"))
+                                        .build())
+                        .build();
+      }
+    }
 
-    Single<Expression> lhsSingle = this.transformExpression(filter.getLhs());
+    Single<Expression> lhsSingle = this.transformExpression(lhsExpression);
     Single<Expression> rhsSingle = this.transformExpression(filter.getRhs());
     Single<List<Filter>> childFilterListSingle =
         Observable.fromIterable(filter.getChildFilterList())
